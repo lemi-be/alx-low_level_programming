1adadee268d791a44648f26f16fb61f822d6aabe
Lemi Bekele <lemibekele365@gmail.com>
2023-02-17 13:30:15 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,35 +1,20 @@
-#include <stdio.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <time.h>
 
+  /**
+  * main - Prints the alphabet in lowercase.
+  *
+  * Return: Always 0.
+  */
 
-
-/**
- *
- *  * main - Prints the alphabet in lowercase.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-int main(void)
-
-{
-
-	 char letter;
-
-
+  int main(void)
+  {
+          char letter;
 
 	  for (letter = 'a'; letter <= 'z'; letter++)
-
 		    putchar(letter);
+	            putchar('\n');
 
-
-
-	   putchar('\n');
-
-
-
-	    return (0);
-
-}
+	   return (0);
+  }
