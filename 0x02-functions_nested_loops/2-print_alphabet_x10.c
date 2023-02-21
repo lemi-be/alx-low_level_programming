4a3d40c557e8e49e21eef59b9c9fb7ce431337aa
Lemi Bekele <lemibekele365@gmail.com>
2023-02-21 13:05:06 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/*
+
+/**
  * print_alphabet_x10 - prints 10 the alphabet, in lowercase,
  * followed by new line
  */
@@ -8,7 +9,7 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	i=0;
+	i = 0;
 
 	while (i < 10)
 	{
