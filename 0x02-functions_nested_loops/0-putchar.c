34322c6392a4be215d6cc3e751a117b36c2a66be
Lemi Bekele <lemibekele365@gmail.com>
2023-02-21 11:43:17 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,8 @@
 int main()
 {
 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('put');
+	_putchar('cha');
 	_putchar('r');
 	_putchar('\n');
 	return 0;
