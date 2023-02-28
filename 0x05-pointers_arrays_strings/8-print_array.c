21b032653ba4950e5a7dea4af2b5794300fe4eab
Lemi Bekele <lemibekele365@gmail.com>
2023-02-27 18:29:37 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,7 +5,7 @@
  * print_array - prints n elements of an array of integers
  * followed by a new line
  * @a: array to be printed
- * @n: number of elements to print
+ * @x: number of elements to print
  */
 void print_array(int *a, int x)
 {
